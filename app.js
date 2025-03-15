32b4363b673ade470f335947f1d34d5d1dddccf0
Vinicius Areias <viareias@gmail.com>
2025-03-14 23:09:54 -0300

    teste 03

--- app.js
+++ app.js
@@ -1,47 +1,44 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
 //Passo 01: Crie um array para armazenar os nomes
-let amigos = [];
+//let amigos = [];
 
 //Passo 02: Implementa uma função para agregar amigos
 function agregarAmigo() {
     //Tarefa 01: Capturar o valor do campo de entrada
-    const inputAmigo = document.getElementById("amigo");
+    let inputAmigo = document.getElementById("amigo");
+    let nomeAmigo = inputAmigo.value.trim();
+    let listaAmigos = document.getElementById("listaAmigos");
     //Tarefa 02: Validar a entrada
-    const nomeAmigo = inputAmigo.value.trim();
+    if(nomeAmigo !== "") {
+        let nomesExistentes = Array.from(listaAmigos.children).map(li=> li.textContent.toLowerCase());
     //Tarefa 03: Se estiver vazio, exiba um alerta com a mensagem de erro
-    if(nome == ""){
+        if(!nomesExistentes.includes(nomeAmigo)) {
+            let li = document.createElement("li");
+            li.textContent = nomeAmigo;
+            listaAmigos.appendChild(li);
+            input.value = "";
+        } else {
+            alert("Este nome já está na lista.");
+        }
+    } else {
         alert("Por favor, insira um nome.");
-        return;
     }
-    //Verificar se não há nome duplicado?
-    if(amigos.includes(nomeAmigo)){
-        alert(`O nome ${nomeAmigo}` já está na lista.);
-        return;
-    }
-    //Tarefa 04: Atualizar o array de amigos
-    amigos.push(nomeAmigo);
-    //Tarefa 05: Limpar o campo de entrada
-    inputAmigo.value = "";
-    //Atualizar lista no HTML?
-    atualizarLista();
-
 }
     
 //Passo 03: Implementa uma função para atualizar a lista de amigos
 function atualizarLista() {
+    let listaAtualizada = document.getElementById("listaAmigos");
     //Tarefa 01: Obter o elemento da lista
-    const listaAmigos = document.getElementById("listaAmigos");
     //Tarefa 02: Limpar a lista existente
-    listaAmigos.innerHTML = "";
+    listaAtualizada.innerHTML = "";
     //Tarefa 03: Percorrer o array
-    for(let z = 0; z <amigos.length; z++){
+    for(let i = 0; i < listaAmigos.length; i++) {
         //Tarefa 04: Adicionar elementos à lista
-        const li = document.createElement("li");
-        li.textContent = amigos[z];
-        listaAmigos.appendChild(li);
+        let itemLista = document.createElement("li");
+        itemLista.innerHTML = listaAmigos[i];
+        listaAtualizada.appendChild(itemLista);
     }
-
 }
     
 //Passo 04: Implementa uma função para sortear os amigos
